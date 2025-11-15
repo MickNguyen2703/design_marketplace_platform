f846af32cf3f02243e0677d29d590fc8d4bdcf35
MickNguyen2703 <hung.nguyen200221@vnuk.edu.vn>
2025-11-15 09:29:43 +0700

    Fixing bugs

---
+++ client/src/components/Featured/Featured.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./Featured.scss";
+
+const Featured = () => {
+  const [search, setSearch] = useState("");
+  const navigate = useNavigate();
+
+  const handleSearch = () => {
+    if (search) {
+      navigate(`/gigs?search=${search}`);
+    }
+  };
+
+  return (
+    <div className="featured">
+      <div className="container">
+        <div className="left">
+          <h1>
+            Find the perfect <span style={{ color: "Orange" }}>freelance</span>{" "}
+            services for your business
+          </h1>
+          <div className="search">
+            <div className="searchInput">
+              <img src="./media/search.png" alt="search" />
+              <input
+                type="search"
+                placeholder='Try "website"'
+                onChange={({ target: { value } }) => setSearch(value)}
+              />
+            </div>
+            <button onClick={handleSearch}>Search</button>
+          </div>
+          <div className="popular">
+            <span>Popular:</span>
+            <button>Website Design</button>
+            <button>WordPress</button>
+            <button>Logo Design</button>
+            <button>AI Services</button>
+          </div>
+        </div>
+
+        <div className="right">
+          <img
+            style={{
+              height: "550px",
+              objectFit: "cover",
+            }}
+            src="./media/banner.jpg"
+            alt="banner"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Featured;
